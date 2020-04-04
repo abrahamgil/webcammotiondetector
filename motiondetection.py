fbf05116a8598eeae3bfe5dedccf09ecea45aa28
Abraham <abrahamgil2@gmail.com>
2020-04-04 07:07:28 -0500

    added face detection contour line to draw a rectangle among every face

--- motiondetection.py
+++ motiondetection.py
@@ -15,15 +15,21 @@ while True:
         continue
     
     delta_frame=cv2.absdiff(first_frame,gray)
-
     thresh_frame=cv2.threshold(delta_frame, 30, 255, cv2.THRESH_BINARY)[1]
+    thresh_frame=cv2.dilate(thresh_frame, None, iterations=2)
 
-    
+    (cnts,_) =cv2.findContours(thresh_frame.copy(),cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
+    for contour in cnts:
+        if cv2.contourArea(contour) < 1000:
+            continue
+        (x, y, w, h)=cv2.boundingRect(contour)
+        cv2.rectangle(frame, (x,y),(x+w, y+h), (0,255,0), 3)
 
     cv2.imshow("Gray Frame", gray)
     cv2.imshow("Delta Frame", delta_frame)
     cv2.imshow("Threshold Frame", thresh_frame)
+    cv2.imshow("Color Frame", frame)
 
 
 
